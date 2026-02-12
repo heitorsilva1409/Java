790454ba860e73cc52b4dd36d7225045b345d7de
Heitor Meirelles Alves da Silva <heitor.alves@foursys.com.br>
2026-02-12 14:47:36 -0300

    Exercício entregável média aluno N notas

--- FCJC05T05E2/src/FCJC05T05E2.java
+++ FCJC05T05E2/src/FCJC05T05E2.java
@@ -4,14 +4,12 @@ public class FCJC05T05E2 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        boolean condicao = true;
         float totalNota = 0;
         float mediaFinal;
         float nota;
         int contador = 0;
-        int qtdInvalidas = 0;
 
-        while (condicao) {
+        while (true) {
             nota = entrada.nextFloat();
             entrada.nextLine();
 
@@ -19,18 +17,14 @@ public class FCJC05T05E2 {
                 totalNota += nota;
                 contador += 1;
             }
-            else if (nota > 10) {
-                qtdInvalidas += 1;
-                break;
-            }
             else {
                 break;
             }
         }
-        if (qtdInvalidas > 0) {
+        if (nota > 10) {
             System.out.println("Nota inválida");
         }
-        else if(qtdInvalidas == 0 && contador != 0) {
+        else if(contador != 0) {
             mediaFinal = totalNota / contador;
             System.out.printf("A média final do aluno é: %.2f", mediaFinal);
         }
