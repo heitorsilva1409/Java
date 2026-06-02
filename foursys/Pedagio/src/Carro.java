c1c17c42e2ffe7d210020b8b0547386fcfeadc6a
heitorsilva1409 <hemeirelles01010@gmail.com>
2026-06-02 16:04:34 -0300

    Organizando as pastas

--- foursys/Pedagio/src/Carro.java
+++
@@ -1,9 +0,0 @@
-public class Carro extends Veiculo{
-    public Carro(String placa, String modelo, int qntEixos) {
-        super(placa, modelo, qntEixos);
-    }
-    @Override
-    public double calcularTarifa (){
-        return 12.00;
-    }
-}
--- foursys/Pedagio/src/Moto.java
+++
@@ -1,9 +0,0 @@
-public class Moto extends Veiculo{
-    public Moto(String placa, String modelo, int qntEixos) {
-        super(placa, modelo, qntEixos);
-    }
-    @Override
-    public double calcularTarifa (){
-        return 5.00;
-    }
-}
